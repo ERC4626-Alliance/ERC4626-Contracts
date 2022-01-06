a850673245b44b5f1e75869c8076307b44320430
Joey Santoro <joseph.santoro@duke.edu>
2022-01-06 14:48:42 -0800

    interfaces

---
+++ src/IERC4626.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.10;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+
+abstract contract IERC4626 is ERC20 {
+
+    /*///////////////////////////////////////////////////////////////
+                                Events
+    //////////////////////////////////////////////////////////////*/
+
+    event Deposit(address indexed from, addres indexed to, uint256 value);
+
+    event Withdraw(address indexed from, addres indexed to, uint256 value);
+
+    /*///////////////////////////////////////////////////////////////
+                            Mutable Functions
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+      @notice Deposit a specific amount of underlying tokens.
+      @param to The address to receive shares corresponding to the deposit
+      @param underlyingAmount The amount of the underlying token to deposit.
+      @return shares The shares in the vault credited to `to`
+    */
+    function deposit(address to, uint256 underlyingAmount) public virtual returns (uint256 shares);
+
+    /**
+      @notice Withdraw a specific amount of underlying tokens.
+      @param to The address to receive underlying corresponding to the withdrawal.
+      @param underlyingAmount The amount of the underlying token to withdraw.
+      @return shares The shares in the vault burned from sender
+    */
+    function withdraw(address to, uint256 underlyingAmount) public virtual returns (uint256 shares);
+
+    /**
+      @notice Withdraw a specific amount of underlying tokens on behalf of `from`.
+      @param from The address to debit shares from corresponding to the withdrawal.
+      @param to The address to receive underlying corresponding to the withdrawal.
+      @param underlyingAmount The amount of the underlying token to withdraw.
+      @return shares The shares in the vault burned from `from`.
+      @dev requires ERC-20 approval of the ERC-4626 shares by sender.
+    */
+    function withdrawFrom(address from, address to, uint256 underlyingAmount) public virtual returns (uint256 shares);
+
+    /**
+      @notice Redeem a specific amount of shares for underlying tokens.
+      @param to The address to receive underlying corresponding to the redemption.
+      @param shareAmount The amount of shares to redeem.
+      @return value The underlying amount transferred to `to`.
+    */
+    function redeem(address to, uint256 shareAmount) public virtual returns (uint256 value);
+
+    /**
+      @notice Redeem a specific amount of shares for underlying tokens on behalf of `from`.
+      @param from The address to debit shares from corresponding to the redemption.
+      @param to The address to receive underlying corresponding to the redemption.
+      @param shareAmount The amount of shares to redeem.
+      @return value The underlying amount transferred to `to`.
+    */
+    function redeemFrom(address from, address to, uint256 underlyingAmount) public virtual returns (uint256 shares);
+
+    /*///////////////////////////////////////////////////////////////
+                            View Functions
+    //////////////////////////////////////////////////////////////*/
+
+    /** 
+      @notice The underlying token the Vault accepts.
+      @return the ERC20 underlying implementation address.
+    */
+    function underlying() public view returns(ERC20);
+
+    /** 
+      @notice Returns a user's Vault balance in underlying tokens.
+      @param user The user to get the underlying balance of.
+      @return balance The user's Vault balance in underlying tokens.
+    */
+    function balanceOfUnderlying(address user) public view returns (uint256 balance);
+
+    /** 
+      @notice Calculates the total amount of underlying tokens the Vault holds.
+      @return totalUnderlyingHeld The total amount of underlying tokens the Vault holds.
+    */
+    function totalHoldings() public view virtual returns (uint256);
+
+    /** 
+      @notice Calculates the amount of shares corresponding to an underlying amount.
+      @param underlyingAmount the amount of underlying tokens to convert to shares.
+      @return sharesAmount the amount of shares corresponding to a given underlying amount
+    */
+    function calculateShares(uint256 underlyingAmount) public view returns (uint256 sharesAmount);
+    
+    /** 
+      @notice Calculates the amount of underlying corresponding to a share amount.
+      @param sharesAmount the amount of shares to convert to an underlying amount.
+      @return underlyingAmount the amount of underlying corresponding to a given amount of shares.
+    */
+    function calculateUnderlying(uint256 sharesAmount) public view returns (uint256 underlyingAmount);
+}
---
+++ src/IERC4626Router.sol
@@ -0,0 +1,259 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.10;
+
+import "./IERC4626.sol";
+
+/** 
+ @title ERC4626Router Interface
+ @author joeysantoro
+ @notice A canonical router between ERC4626 Vaults <link ERC>
+
+ There are 2 types of Vaults with different access paterns:
+  * "Approve Vaults" which use ERC20 approval to transferFrom the sender to the vault. e.g. Compound cToken
+  * "Optimistic Vaults" which use internal accounting to grant ownership based on new underlying sent to the contract. e.g. Uniswap V2
+
+ The router atomically executes user journeys to originate/close a vault position, or route between ERC4626 vault implementations for the same token.
+ @dev the router makes no special considerations for unique ERC20 implementations such as fee on transfer. 
+ There are no built in protections for unexpected behavior beyond enforcing the minSharesOut is received.
+ */ 
+interface IERC4626Router {
+    /************************** Errors **************************/
+
+    /// @notice thrown when amount of shares/underlying received is below the min set by caller
+    error MinOutError();
+
+    /// @notice thrown when fromVault's underlying is not equal to toVault's underlying
+    error UnderlyingMismatchError();
+
+    /************************** Deposit **************************/
+
+    /** 
+     @notice deposit underlying to an "Approve Vault"
+     @param vault The ERC4626 vault to deposit shares to
+     @param to The destination of ownership shares
+     @param amountIn The amount of underlying to transfer to the vault
+     @param minSharesOut The min amount of shares received by `to`
+     @return sharesOut the amount of shares received by `to`
+     @dev throws MinOutError
+    */
+    function depositToApproveVault(
+        IERC4626 vault, 
+        address to, 
+        uint256 amountIn,
+        uint256 minSharesOut
+    ) external returns(uint256 sharesOut);
+
+    /** 
+     @notice deposit underlying to an "Optimistic Vault"
+     @param vault The ERC4626 vault to deposit shares to
+     @param to The destination of ownership shares
+     @param amountIn The amount of underlying to transfer to the vault
+     @param minSharesOut The min amount of shares received by `to`
+     @return sharesOut the amount of shares received by `to`
+     @dev throws MinOutError    
+    */
+    function depositToOptimisticVault(
+        IERC4626 vault, 
+        address to, 
+        uint256 amountIn,
+        uint256 minSharesOut
+    ) external returns(uint256 sharesOut);
+    
+    /************************** Withdraw **************************/
+
+    /** 
+     @notice withdraw `underlying` to an Approve Vault.
+     @param fromVault The ERC4626 vault to withdraw underlying from
+     @param toVault The ERC4626 vault to deposit underlying to
+     @param to The destination of ownership shares
+     @param amountUnderlying The amount underlying to withdraw and deposit
+     @param minSharesOut The min amount of shares received by `to`
+     @return sharesOut the amount of shares received by `to`
+     @dev throws MinOutError, UnderlyingMismatchError   
+    */
+    function withdrawToApproveVault(
+        IERC4626 fromVault, 
+        IERC4626 toVault,
+        address to, 
+        uint256 amountUnderlying,
+        uint256 minSharesOut
+    ) external returns(uint256 sharesOut);
+
+    /** 
+     @notice withdraw `underlying` to an Approve Vault.
+     @param fromVault The ERC4626 vault to withdraw underlying from
+     @param toVault The ERC4626 vault to deposit underlying to
+     @param to The destination of ownership shares
+     @param amountUnderlying The amount underlying to withdraw and deposit
+     @param minSharesOut The min amount of shares received by `to`
+     @return sharesOut the amount of shares received by `to`
+     @dev throws MinOutError, UnderlyingMismatchError   
+    */
+    function withdrawToOptimisticVault(
+        IERC4626 fromVault, 
+        IERC4626 toVault,
+        address to, 
+        uint256 amountUnderying,
+        uint256 minSharesOut
+    ) external returns(uint256 sharesOut);
+
+
+    /************************** Redeem **************************/
+
+    /** 
+     @notice redeem `shares` to an Approve Vault.
+     @param fromVault The ERC4626 vault to withdraw underlying from
+     @param toVault The ERC4626 vault to deposit underlying to
+     @param to The destination of ownership shares
+     @param amountShares The amount of shares to redeem and deposit
+     @param minSharesOut The min amount of shares received by `to`
+     @return sharesOut the amount of shares received by `to`
+     @dev throws MinOutError, UnderlyingMismatchError   
+    */
+    function redeemToApproveVault(
+        IERC4626 fromVault, 
+        IERC4626 toVault,
+        address to, 
+        uint256 amountShares,
+        uint256 minSharesOut
+    ) external returns(uint256 sharesOut);
+
+    /** 
+     @notice redeem `shares` to an Optimistic Vault.
+     @param fromVault The ERC4626 vault to withdraw underlying from
+     @param toVault The ERC4626 vault to deposit underlying to
+     @param to The destination of ownership shares
+     @param amountShares The amount of shares to redeem and deposit
+     @param minSharesOut The min amount of shares received by `to`
+     @return sharesOut the amount of shares received by `to`
+     @dev throws MinOutError, UnderlyingMismatchError   
+    */
+    function redeemToOptimisticVault(
+        IERC4626 fromVault, 
+        IERC4626 toVault,
+        address to, 
+        uint256 amountShares,
+        uint256 minSharesOut
+    ) external returns(uint256 sharesOut);
+
+    /*/////////////////////////////////////////////////////////////
+                            Permit Implementations
+    /////////////////////////////////////////////////////////////*/
+
+    /************************** Deposit **************************/
+
+    /** 
+     @notice deposit underlying to an "Approve Vault" using EIP-2612 permit
+     @param vault The ERC4626 vault to deposit shares to
+     @param to The destination of ownership shares
+     @param amountIn The amount of underlying to transfer to the vault
+     @param minSharesOut The min amount of shares received by `to`
+     @return sharesOut the amount of shares received by `to`
+     @dev throws MinOutError
+    */
+    function depositWithPermitToApproveVault(
+        IERC4626 vault, 
+        address to, 
+        uint256 amountIn,
+        uint256 minSharesOut,
+        uint deadline, bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns(uint256 sharesOut);
+
+    /** 
+     @notice deposit underlying to an "Optimistic Vault" using EIP-2612 permit
+     @param vault The ERC4626 vault to deposit shares to
+     @param to The destination of ownership shares
+     @param amountIn The amount of underlying to transfer to the vault
+     @param minSharesOut The min amount of shares received by `to`
+     @return sharesOut the amount of shares received by `to`
+     @dev throws MinOutError    
+    */
+    function depositWithPermitToOptimisticVault(
+        IERC4626 vault, 
+        address to, 
+        uint256 amountIn,
+        uint256 minSharesOut,
+        uint deadline, bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns(uint256 sharesOut);
+    
+    /************************** Withdraw **************************/
+
+    /** 
+     @notice withdraw `underlying` to an Approve Vault using EIP-2612 permit
+     @param fromVault The ERC4626 vault to withdraw underlying from
+     @param toVault The ERC4626 vault to deposit underlying to
+     @param to The destination of ownership shares
+     @param amountUnderlying The amount underlying to withdraw and deposit
+     @param minSharesOut The min amount of shares received by `to`
+     @return sharesOut the amount of shares received by `to`
+     @dev throws MinOutError, UnderlyingMismatchError   
+    */
+    function withdrawWithPermitToApproveVault(
+        IERC4626 fromVault, 
+        IERC4626 toVault,
+        address to, 
+        uint256 amountUnderlying,
+        uint256 minSharesOut,
+        uint deadline, bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns(uint256 sharesOut);
+
+    /** 
+     @notice withdraw `underlying` to an Approve Vault using EIP-2612 permit
+     @param fromVault The ERC4626 vault to withdraw underlying from
+     @param toVault The ERC4626 vault to deposit underlying to
+     @param to The destination of ownership shares
+     @param amountUnderlying The amount underlying to withdraw and deposit
+     @param minSharesOut The min amount of shares received by `to`
+     @return sharesOut the amount of shares received by `to`
+     @dev throws MinOutError, UnderlyingMismatchError   
+    */
+    function withdrawWithPermitToOptimisticVault(
+        IERC4626 fromVault, 
+        IERC4626 toVault,
+        address to, 
+        uint256 amountUnderying,
+        uint256 minSharesOut,
+        uint deadline, bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns(uint256 sharesOut);
+
+
+    /************************** Redeem **************************/
+
+    /** 
+     @notice redeem `shares` to an Approve Vault using EIP-2612 permit
+     @param fromVault The ERC4626 vault to withdraw underlying from
+     @param toVault The ERC4626 vault to deposit underlying to
+     @param to The destination of ownership shares
+     @param amountShares The amount of shares to redeem and deposit
+     @param minSharesOut The min amount of shares received by `to`
+     @return sharesOut the amount of shares received by `to`
+     @dev throws MinOutError, UnderlyingMismatchError   
+    */
+    function redeemWithPermitToApproveVault(
+        IERC4626 fromVault, 
+        IERC4626 toVault,
+        address to, 
+        uint256 amountShares,
+        uint256 minSharesOut,
+        uint deadline, bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns(uint256 sharesOut);
+
+    /** 
+     @notice redeem `shares` to an Optimistic Vault using EIP-2612 permit
+     @param fromVault The ERC4626 vault to withdraw underlying from
+     @param toVault The ERC4626 vault to deposit underlying to
+     @param to The destination of ownership shares
+     @param amountShares The amount of shares to redeem and deposit
+     @param minSharesOut The min amount of shares received by `to`
+     @return sharesOut the amount of shares received by `to`
+     @dev throws MinOutError, UnderlyingMismatchError   
+    */
+    function redeemWithPermitToOptimisticVault(
+        IERC4626 fromVault, 
+        IERC4626 toVault,
+        address to, 
+        uint256 amountShares,
+        uint256 minSharesOut,
+        uint deadline, bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns(uint256 sharesOut);
+}
