f6f40ce62375b5f3bc204c7ff8eb926efb32921f
Erwan Beauvois <erwan@allmyt.eu>
2022-02-23 17:30:29 +0100

    Add label on Hevm

--- src/test/Hevm.sol
+++ src/test/Hevm.sol
@@ -52,6 +52,8 @@ interface Hevm {
     // Expect a call to an address with the specified calldata.
     // Calldata can either be strict or a partial match
     function expectCall(address,bytes calldata) external;
-
+    // Gets the code from an artifact file. Takes in the relative path to the json file
     function getCode(string calldata) external returns (bytes memory);
+    // Labels an address in call traces
+    function label(address, string calldata) external;
 }
